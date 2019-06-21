b34aced48ab4b2a1db285dea74cfa7ccfce7d0f8
samjeg <samjegede@outlook.com>
2019-06-21 17:15:31 +0100

    Working unit tests!

--- cat_app/tests.py
+++ cat_app/tests.py
@@ -9,27 +9,138 @@ from selenium.webdriver.support.wait import WebDriverWait
 from django.contrib.auth.models import User
 from .models import UserProfileInfo, Cat_Topic, Cat_Topic_Comment
 
-class CatFriendsTests(TestCase):
+class RequestTests(TestCase):
+	databases = {"__all__"}
 
-    def test_get_homepage(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+	def test_get_homepage(self):
+		print('test1')
+		response = self.client.get('/')
+		self.assertEqual(response.status_code, 200)
 
-    def test_get_catlist_page(self):
-        response = self.client.get('/cat_app/cat_list/')
-        self.assertEqual(response.status_code, 200)
+	def test_get_catlistpage(self):
+		print('test2')
+		response = self.client.get('/cat_app/cat_list/')
+		self.assertEqual(response.status_code, 200)
 
-    def test_get_loginpage(self):
-        response = self.client.get('/cat_app/login/')
-        self.assertEqual(response.status_code, 200)
+	def test_get_loginpage(self):
+		print('test3')
+		response = self.client.get('/cat_app/login/')
+		self.assertEqual(response.status_code, 200)
 
-    def test_get_register_page(self):
-        response = self.client.get('/cat_app/register/')
-        self.assertEqual(response.status_code, 200)
+	def test_get_registerpage(self):
+		print('test4')
+		response = self.client.get('/cat_app/register/')
+		self.assertEqual(response.status_code, 200)
 
-    def test_create_userprofile(self):
+
+
+class CatTopicCommentTests(TestCase):
+	databases = {"__all__"}
+
+	@classmethod
+	def setUpClass(cls):
+		super(CatTopicCommentTests, cls).setUpClass()
+
+	@classmethod
+	def tearDownClass(cls):
+		super(CatTopicCommentTests, cls).tearDownClass()
+
+	def create_user_missy(self):
+		return User.objects.create_user('Missy', 'testpassword')
+
+	def create_profile_missy(self, user):
+		user_profile = UserProfileInfo.objects.create(
+			user=user,
+			picture="images/cat1.jpg"
+		)
+
+		return user_profile
+
+	def create_cat_lenny(self, user_profile):
+		cat_topic = Cat_Topic.objects.create(
+			owner=user_profile,
+			cat_name="Lenny",
+			cat_picture="cat_images/cat1.jpg",
+			story="This cat is extreemly lazy"
+		)
+
+		return cat_topic
+
+	def test_catcomment(self):
+		print('test7')
+		lengthBefore = len(Cat_Topic_Comment.objects.all())
+
+		user = self.create_user_missy() 
+		user_profile = self.create_profile_missy(user)
+		cat_topic = self.create_cat_lenny(user_profile)
+
+		Cat_Topic_Comment.objects.create(
+			user=user,
+			cat_topic=cat_topic,
+			comment_picture_path="cat_images/cat1.jpg",
+			comment="Yo I have been looking for have you. Have you been on here talking about cats all day?"
+		)
+
+		lengthAfter = len(Cat_Topic_Comment.objects.all())
+
+		self.assertEqual(lengthBefore!=lengthAfter, True)
+
+class CatTopicTests(TestCase):
+	databases = {"__all__"}
+
+	@classmethod
+	def setUpClass(cls):
+		super(CatTopicTests, cls).setUpClass()
+
+	@classmethod
+	def tearDownClass(cls):
+		super(CatTopicTests, cls).tearDownClass()
+	
+	def create_user_micheal(self):
+		return User.objects.create_user('Micheal', 'testpassword')
+
+	def create_profile_micheal(self, user):
+		user_profile = UserProfileInfo.objects.create(
+			user=user,
+			picture="images/cat1.jpg"
+		)
+
+		return user_profile
+
+	def test_cat(self):
+		print('test6')
+		lengthBefore = len(Cat_Topic.objects.all())
+		user = self.create_user_micheal()
+		user_profile = self.create_profile_micheal(user)
+		Cat_Topic.objects.create(
+			owner=user_profile,
+			cat_name="Garfield",
+			cat_picture="cat_images/cat1.jpg",
+			story="This cat is extreemly lazy"
+		)
+
+		lengthAfter = len(Cat_Topic.objects.all())
+
+		self.assertEqual(lengthBefore!=lengthAfter, True)
+
+class UserProfileTests(TestCase):
+	databases = {"__all__"}
+
+	@classmethod
+	def setUpClass(cls):
+		super(UserProfileTests, cls).setUpClass()
+
+	@classmethod
+	def tearDownClass(cls):
+		super(UserProfileTests, cls).tearDownClass()
+
+	def create_user_rubberduck(self):
+		return User.objects.create_user('Rubberduck', 'testpassword')
+	
+	def test_userprofileinfo(self):
+		print('test5')
 		lengthBefore = len(UserProfileInfo.objects.all())
-		user = User.objects.create_user('rubberduck', 'testpassword')
+		user = self.create_user_rubberduck()
 		UserProfileInfo.objects.create(
 			user=user,
 			picture="images/cat1.jpg"
@@ -37,9 +148,10 @@ class CatFriendsTests(TestCase):
 
 		lengthAfter = len(UserProfileInfo.objects.all())
 
-		self.assertIs(lengthBefore!=lengthAfter, True)
+		self.assertEqual(lengthBefore!=lengthAfter, True)
 
 class CatFriendsSeleniumTests(LiveServerTestCase):
+	databases = {"__all__"}
 
 	@classmethod
 	def setUpClass(cls):
@@ -53,6 +165,7 @@ class CatFriendsSeleniumTests(LiveServerTestCase):
 		super(CatFriendsSeleniumTests, cls).tearDownClass()
 
 	def test_login(self):
+		print('test8')
 		self.selenium.get("http://127.0.0.1:8000/cat_app/login/")
 		username_input = self.selenium.find_element_by_name("username")
 		username_input.send_keys('Jilly')
@@ -64,7 +177,8 @@ class CatFriendsSeleniumTests(LiveServerTestCase):
 		self.assertIn("Welcome", body.text)
 
 
-	def test_login_fakeuser(self):
+	def test_loginfakeuser(self):
+		print('test9')
 		self.selenium.get("http://127.0.0.1:8000/cat_app/login/")
 		username_input = self.selenium.find_element_by_name("username")
 		username_input.send_keys('Harry')
