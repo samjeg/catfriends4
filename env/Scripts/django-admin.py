99c887c85143e3b04c3b95ac56b49359be6221a6
samjeg <samjegede@outlook.com>
2019-06-22 22:06:50 +0100

    added update jenkins

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\samje\documents\webprojects2\djangoenv\catfriends4\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
